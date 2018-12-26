6e9a3c83ee0a8d3e19ed0c268d95265151140895
ozihler <oliver.zihler@zuehlke.com>
2018-12-26 12:31:46 +0100

    add back links

--- src/main/resources/update-all-dependencies.bat
+++ src/main/resources/update-all-dependencies.bat
@@ -1,9 +1,9 @@
 echo Updating dependencies in package.json
-cd C:\Users\olzi\workspace\fish\frontend
+cd C:\Users\olzi\workspace\qa\frontend
 echo npm i -g npm-check-updates
-call npm i -g npm-check-updates
+rem call npm i -g npm-check-updates
 echo ncu -ua (gets all the new dependencies and puts it into package.json)
-call ncu -ua
+rem call ncu -ua
 echo npm install
 call npm install
 echo done updating dependencies in package.json
