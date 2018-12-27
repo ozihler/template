95e78ad9c74d9d5f4c891bb10947e30ce5dcb49f
ozihler <oliver.zihler@zuehlke.com>
2018-12-27 12:00:44 +0100

    Add additional environment, remove all traces of material.

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {environment} from "../environments/environment";
 
 @Component({
   selector: 'root',
@@ -6,5 +7,6 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
 
+  name: string = environment.name;
 
 }
