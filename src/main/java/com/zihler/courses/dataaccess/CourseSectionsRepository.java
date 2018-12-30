f02ff6e7bd74e970ce33ecfce5a35b63b829a430
ozihler <oliver.zihler@zuehlke.com>
2018-12-30 23:25:41 +0100

    add todo

--- src/main/java/com/zihler/courses/dataaccess/CourseSectionsRepository.java
+++ src/main/java/com/zihler/courses/dataaccess/CourseSectionsRepository.java
@@ -4,6 +4,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
+// TODO: 30.12.2018 Course Sections should be updated independent of Course! Also in Frontend
 public interface CourseSectionsRepository extends JpaRepository<CourseSection, Long> {
     List<CourseSection> findByCourseId(Long courseId);
 }
