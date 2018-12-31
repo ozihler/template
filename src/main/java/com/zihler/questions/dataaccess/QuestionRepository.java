8ee8d3a356d992a5da448eb7ee6801397b620ffa
ozihler <oliver.zihler@zuehlke.com>
2018-12-31 18:19:57 +0100

    remove old repos

--- src/main/java/com/zihler/questions/dataaccess/QuestionRepository.java
+++
@@ -1,9 +0,0 @@
-package com.zihler.questions.dataaccess;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface QuestionRepository extends JpaRepository<QuestionEntity, Long> {
-
-}
